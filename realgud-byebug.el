f8f20b92c6b13f75cc9797921c0e28d3def48b1c
rocky <rocky@gnu.org>
2019-05-20 07:40:53 -0400

    Small package-lint stuff

--- realgud-byebug.el
+++ realgud-byebug.el
@@ -3,7 +3,7 @@
 ;; Author: Rocky Bernstein
 ;; Version: 1.0.0
 ;; Package-Type: multi
-;; Package-Requires: ((realgud "1.4.3") (cl-lib "0.5") (emacs "24"))
+;; Package-Requires: ((realgud "1.4.5") (load-relative "1.2") (cl-lib "0.5") (emacs "24"))
 ;; URL: http://github.com/rocky/realgud-byebug
 ;; Compatibility: GNU Emacs 24.x
 
